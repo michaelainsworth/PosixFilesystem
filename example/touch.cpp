93f3ae0070b00f18d42be421687c6623c97151d0
Michael Ainsworth <michael@michaelainsworth.id.au>
2017-03-23 23:19:28 +1100

    Added documentation and renamed example file.

--- example/touch.cpp
+++ example/touch.cpp
@@ -13,6 +13,8 @@ int main(int argc, const char**argv)
 {
     Directory directory(".");
 
+    // Loop over all the file names specified on the command
+    // line, creating files as appropriate.
     for (int i = 1; i < argc; ++i)
     {
         directory.file(argv[i], Write);
